3d6f19baa1e3f7c1c28c03b03e8d9af4f9d502df
jgordor <jose.gordo@tractis.com>
2010-06-24 15:08:12 +0200

    Option for not include association for filter fields, this can be used with custom conditions to do complex "No SQL" filters

---
+++ acts_as_criteria.rb
@@ -0,0 +1,235 @@
+require 'rubygems'
+require 'activerecord'
+
+module ActsAsCriteria   
+  def acts_as_criteria(*args)        
+    options = args.extract_options!
+    simple_options = options[:simple]
+    filter_options = options[:filter]
+    
+    # Check for others plugins adding filters    
+    plugins_path = "#{RAILS_ROOT}/vendor/plugins"
+    filter_path = "config/criteria_filter.rb"
+    
+    Dir.foreach("#{RAILS_ROOT}/vendor/plugins") do |plugin| 
+      if File.exist?("#{plugins_path}/#{plugin}/#{filter_path}")
+        load "#{plugins_path}/#{plugin}/#{filter_path}"
+        plugin_filters = send("get_#{plugin}_criteria_filters")
+        plugin_filters.each do |filter|
+          filter_options[:columns].merge!(filter[:filters]) if filter[:model] == self
+        end
+      end      
+    end
+    
+    # defaults
+    simple_options[:match] ||= :start
+    options[:named] ||= 'search'    
+    
+    class_inheritable_accessor  :criteria_options
+    write_inheritable_attribute :criteria_options, options
+    
+    named_scope options[:named], lambda { |terms|
+      if terms.instance_of? String
+        terms = simple_options[:escape].call(terms) if simple_options[:escape]
+        simple(terms, simple_options[:columns], simple_options[:match]) if terms.instance_of? String
+      else
+        filter(terms, filter_options)
+      end
+    }
+  end
+  
+  def filter(terms, options)
+    conds, assocs  = [], []
+    options[:columns].each do |col, opts|
+      unless terms[col].blank?
+	assocs << get_col_assoc(col, opts) if !opts[:no_assoc] == true && col_needs_assoc(col, terms)
+        col_name = get_col_name(col)
+        unless terms[col][:value].blank?
+          if terms[col][:value].size > 1
+            ored_cond = []
+            terms[col][:value].each do |col_value|
+              ored_term = { "match" => terms[col][:match], "value" => col_value }
+              ored_cond <<  get_condition(col_name, col, ored_term, opts) if check_data(col, col_value)
+            end
+            conds << merge_conditions(*ored_cond.join(" OR "))
+          else
+            conds << get_condition(col_name, col, terms[col], opts) if check_data(col, terms[col][:value].first)
+          end
+        end
+      end
+    end
+    
+    conditions = merge_conditions(*conds.join(" AND "))
+    { :conditions => conditions, :include => assocs }
+  end
+
+  def get_condition(col_name, col, term, opts)
+    if opts[:condition]
+      opts[:condition].call(term["value"], self)
+    else
+      [ "#{get_colname(col, col_name)} #{get_pattern(col, term)}" ]
+    end
+  end
+
+  def get_colname(col, col_name)
+    unless col_subtype(col) == :period
+      col_name
+    else
+      "DATE(#{col_name})"
+    end
+    
+  end
+
+  def check_data(col, value)
+    case col_subtype(col)
+      when :text
+        true
+      when :num
+        is_numeric?(value)
+      when :period
+        Date.parse(value) rescue false
+      when :bool
+        is_numeric?(value)
+    end
+  end
+
+  def is_numeric?(term)
+    true if Float(term) rescue false
+  end
+
+  def get_col_assoc(col, opts)
+    opts[:relation_name] || col.to_s.split(".").first.to_sym
+  end
+
+  def col_needs_assoc(col, terms)
+    !column_names.include?(col.to_s.split(".").first) && !terms[col][:value].blank?
+  end
+
+  def get_col_name(col)
+    column_names.include?(col.to_s.split(".").first) ? "#{table_name}.#{col}" : col
+  end
+
+  def col_subtype(col)
+    col_parts = col.to_s.split(".")
+    col_type = col_parts.size == 1 ? columns_hash["#{col}"].type : col_parts.first.singularize.camelize.constantize.columns_hash["#{col_parts.last}"].type
+    
+    case col_type
+      when :string, :text
+        then :text
+      when :integer, :float, :decimal
+        then :num
+      when :datetime, :date
+        then :period
+      when :boolean
+        then :bool
+      else
+        raise "Column type not supported: #{col_type}"
+    end
+  end
+  
+  def get_pattern(col, term) 
+    # first term conversion and checks
+    match = term["match"]
+    value = term["value"].class == Array ? term["value"].first : term["value"]
+    
+    self.send(:"get_#{col_subtype(col).to_s}_pattern", { :match => match, :value => value })
+  end
+  
+  def get_text_pattern(term)    
+    term[:match] ||= :contains
+    like = connection.adapter_name == "PostgreSQL" ? "ILIKE" : "LIKE"
+    
+    case term[:match].to_sym
+      when :is
+        "= '#{term[:value]}'"
+      when :is_not
+        "!= '#{term[:value]}'"        
+      when :begin
+        "#{like} '#{term[:value]}%'"
+      when :contains
+        "#{like} '%#{term[:value]}%'"
+      when :not_contains
+        "NOT #{like} '%#{term[:value]}%'"        
+      when :end
+        "#{like} '%#{term[:value]}'"
+      else
+        raise "Unexpected match type: #{term[:match]}"
+    end     
+  end
+  
+  def get_num_pattern(term)
+    term[:match] ||= :eq
+    
+    case term[:match].to_sym
+      when :eq
+        "= #{term[:value]}"
+      when :ne
+        "<> #{term[:value]}"
+      when :gt
+        "> #{term[:value]}"
+      when :ge
+        ">= #{term[:value]}"  
+      when :lt
+        "< #{term[:value]}"
+      when :le
+        "<= #{term[:value]}"     
+      else
+        raise "Unexpected match type: #{term[:match]}"        
+    end
+  end
+  
+  def get_period_pattern(term)
+    term[:match] ||= :eq
+    value = Time.parse(term[:value]).strftime("%Y-%m-%d")
+
+    case term[:match].to_sym
+      when :eq
+        "= '#{value}'"
+      when :ne
+        "<> '#{value}'"
+      when :gt
+        "> '#{value}'"
+      when :ge
+        ">= '#{value}'"
+      when :lt
+        "< '#{value}'"
+      when :le
+        "<= '#{value}'"
+      else
+        raise "Unexpected match type: #{term[:match]}"        
+    end    
+  end
+  
+  def get_bool_pattern(term)
+    term[:value].to_s == "1" ? "= 1" : "= 0"
+  end
+  
+  def simple(terms, columns, match)
+    # PostgreSQL LIKE is case-sensitive, use ILIKE for case-insensitive
+    like = connection.adapter_name == "PostgreSQL" ? "ILIKE" : "LIKE"
+
+    conditions = terms.split.inject(nil) do |acc, term|
+      pattern = get_like(match, term)
+      merge_conditions acc, [columns.collect { |column| "#{table_name}.#{column} #{like} :pattern" }.join(' OR '), { :pattern => pattern }]
+    end
+
+    { :conditions => conditions }    
+  end
+  
+  def get_like(match, term)
+    match ||= :contains
+    case(match)
+    when :is
+      term
+    when :begin
+      term + '%'
+    when :contains
+      '%' + term + '%'
+    when :end
+      '%' + term
+    else
+      raise "Unexpected match type: #{match}"
+    end    
+  end
+  
+end
---
+++ acts_as_criteria_helpers.rb
@@ -0,0 +1,196 @@
+module ActsAsCriteria 
+  module FormHelper
+    def acts_as_criteria_form(method, model, options={})
+      case method
+        when :simple
+          simple(model, options) 
+        when :filter
+          filter(model, options)
+        else
+          raise "Unexpected method type: #{method}, use :simple or :filter"          
+      end
+    end
+    
+    def simple(model, options)
+      multi = model.is_a?(Array) ? true : false
+      options[:class]  ||= :search
+      options[:method] ||= :get
+      if multi == true
+        options[:actions] = []
+        model.each do |m|          
+          options[:actions] << [  m.to_s.pluralize, self.send("search_#{m.to_s.downcase.pluralize}_path") ]
+        end
+      else
+        options[:action] ||= self.send("search_#{model.to_s.downcase.pluralize}_path")
+      end
+      options[:label] ||= acts_as_criteria_get_translation(model, "search")
+      render :partial => "acts_as_criteria/simple", :locals => { :options => options, :multi => multi, :model => model }
+    end
+    
+    def filter(model, options)      
+      filters = model.criteria_options[:filter]      
+      
+      raise "Unsupported method filter for model #{model.to_s}" if filters.blank?
+      
+      options[:class]  ||= :search
+      options[:method] ||= :get
+      options[:action] ||= self.send("search_#{model.to_s.downcase.pluralize}_path")
+      options[:label] ||= acts_as_criteria_get_translation(model, "filter")
+      
+      render :partial => "acts_as_criteria/filter", :locals => { :options => options, :model => model, :columns => options_for_columns(filters) }
+    end
+
+    def options_for_columns(filters)      
+      filters[:columns].map { |col, val| [ acts_as_criteria_get_translation(acts_as_criteria_get_current_model, val[:text]||col), col ] }.insert(0, "")
+    end
+    
+    def acts_as_criteria_get_translation(model, text)
+      # Check for internalization support
+      if model.criteria_options[:i18n]
+        model.criteria_options[:i18n].call(text)
+      else
+        text
+      end
+    end
+
+    def acts_as_criteria_get_current_model
+      controller_name.singularize.camelize.constantize
+    end
+
+    def acts_as_criteria_input_label(col, col_options)
+      if col_options[:text]
+        acts_as_criteria_get_translation(acts_as_criteria_get_current_model, col_options[:text])
+      else
+        acts_as_criteria_get_translation(acts_as_criteria_get_current_model, col)
+      end
+    end
+
+    def acts_as_criteria_input_operator(col_subtype, col, current_query, col_options)      
+      current_match = (current_query.blank? || current_query[col].blank? || current_query[col][:match].blank?) ? "" : current_query[col][:match]
+      model = acts_as_criteria_get_current_model
+
+      case col_subtype
+        when :text then 
+          if col_options[:source].blank?  
+            opts = [[acts_as_criteria_get_translation(model,"contains"),"contains"], [acts_as_criteria_get_translation(model,"doesnt_contains"), "not_contains"],
+                    [acts_as_criteria_get_translation(model,"begins_with"),"begin"], [acts_as_criteria_get_translation(model,"ends_with"),"end"],
+                    [acts_as_criteria_get_translation(model,"is"),"is"], [acts_as_criteria_get_translation(model,"is_not"),"is_not"]]
+          else
+            opts = [[acts_as_criteria_get_translation(model,"contains"),"contains"], [acts_as_criteria_get_translation(model,"doesnt_contains"), "not_contains"]]
+          end            
+        when :num, :period then 
+          if col_options[:source].blank?
+            opts = [["=","eq"], ["<>","ne"], [">","gt"], [">=","ge"], ["<","lt"], ["<=","le"]]
+          else
+            opts = [["=","eq"], ["<>","ne"]]
+          end
+        when :bool
+          then return ""
+        else
+          raise "Column subtype not supported: #{col_subtype}"
+      end
+      select_tag :"query[#{col}][match]", options_for_select(opts, current_match)
+    end
+    
+    def acts_as_criteria_input_field(col_subtype, col, current_query, filter_val)
+      size = 32
+      unless filter_val.blank?
+        current_value = filter_val
+      else
+        current_value = (current_query.blank? || current_query[col].blank? || current_query[col][:value].blank?) ? "" : current_query[col][:value].first
+      end      
+      input_name = :"query[#{col}][value][]"
+      case col_subtype
+        when :text, :num
+          then text_field_tag(input_name, current_value, :id => nil, :size => size)
+        when :period
+          then 
+            if self.respond_to? "calendar_date_select_tag"
+              calendar_date_select_tag(input_name, current_value, :id => nil, :size => size - 5)
+            else
+              text_field_tag(input_name, current_value, :id => nil, :size => size)
+            end
+        when :bool
+          then select_tag input_name, options_for_select([["False","0"], ["True","1"]], current_value)
+        else
+          raise "Column subtype not supported: #{col_subtype}"    
+      end
+    end
+    
+    def acts_as_criteria_input_source(col_subtype, col, source, current_query, filter_val)
+      unless filter_val.blank?
+        current_value = filter_val
+      else
+        current_value = (current_query.blank? || current_query[col].blank? || current_query[col][:value].blank?) ? "" : current_query[col][:value].first
+      end       
+      current_value = current_value.to_i if Float(current_value) rescue false
+
+      options = source.call(options)
+      select_tag :"query[#{col}][value][]", options_for_select(options, current_value), { :style => "width: 250px;" }
+    end
+    
+    def acts_as_criteria_set_visibility(type, current_query, options = {})
+      case type
+        when :simple then
+          # Enabling the simple search input if query not active or is string
+          if current_query.blank? || current_query.instance_of?(String)
+            return false
+          else
+            return true
+          end
+        when :filter then
+          # Hide the filters form if hidden option is set and active query is not a filter
+          if options[:hidden] && !current_query.instance_of?(HashWithIndifferentAccess)
+            return "style='display:none;'"
+          else
+            return ""
+          end
+        else
+          raise "Type not supported: #{type}, use :simple or :filter"
+      end
+    end
+    
+    def acts_as_criteria_get_action_link(action, type)
+      if type[:text]
+        link_name = acts_as_criteria_get_translation(acts_as_criteria_get_current_model, type[:text])
+      else
+        link_name = image_tag(type[:image])
+      end
+      link_to_remote link_name, :url => { :action => :criteria, :id => action }
+    end
+    
+    def acts_as_criteria_is_filter_active(current_query)
+      current_query.instance_of?(HashWithIndifferentAccess)
+    end
+
+    def acts_as_criteria_select_user_filters(current_user, text = "select_existing", path = nil, autosubmit = true)
+      path ||= send("search_#{controller_name}_path")
+      filters = UserFilter.find(:all, :conditions => { :user_id => current_user, :asset => controller_name })
+      options = filters.map{ |filter| [filter.name, filter.criteria] }.insert(0, acts_as_criteria_get_translation(acts_as_criteria_get_current_model, text))
+      onchange = autosubmit == true ? "document.location = '#{path}?' + this.value" : ""
+      select_tag "criteria_select_filter", options_for_select(options, 0), :onchange => onchange
+    end
+
+    def acts_as_criteria_save_user_filter_form(current_user)
+      filters = UserFilter.find(:all, :conditions => { :user_id => current_user, :asset => controller_name })
+      options = filters.map{ |filter| [filter.name, filter.id] }.insert(0, [acts_as_criteria_get_translation(acts_as_criteria_get_current_model, "select_one"), ""])
+
+      form = []
+
+      form << "<br />"
+      form << form_remote_tag(:url => { :action => :criteria, :id => "save_filters" })
+      form << hidden_field_tag("user_id", current_user)
+      form << "<strong>#{acts_as_criteria_get_translation(acts_as_criteria_get_current_model, "save_as_new_filter")}:</strong><br />"
+      form << "#{acts_as_criteria_get_translation(acts_as_criteria_get_current_model, "name")}: #{text_field_tag("filter_name", nil, :size => 15, :id => "acts_as_criteria_filter_name")}"
+      form << "#{acts_as_criteria_get_translation(acts_as_criteria_get_current_model, "description")}: #{text_field_tag("filter_description", nil, :size => 35, :id => "acts_as_criteria_filter_description")}"
+      form << "<br />"
+      form << "<strong>#{acts_as_criteria_get_translation(acts_as_criteria_get_current_model, "or_ovewrite_existing")}</strong>"
+      form << select_tag("criteria_select_filter", options_for_select(options, 0))
+      form << "<br /><br />"
+      form << submit_tag("#{acts_as_criteria_get_translation(acts_as_criteria_get_current_model, "save")}")
+
+      form.join("\n")
+    end
+  end
+ 
+end
